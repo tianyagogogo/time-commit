37a9aa8005d400cfe257b2a93573ba86afd1f47e
tianya <24786545+tianyagogogo@users.noreply.github.com>
2023-01-29 15:48:11 +0800

    git commit

--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-cd /root/time-commit && git config --system user.email "24786545+tianyagogogo@users.noreply.github.com" && git config --system user.name "tianyagogogo"  && git pull && echo $(date) > records.txt && git commit -a -m 'git commit '  && git push origin main
+cd /root/time-commit && git config --system user.email "24786545+tianyagogogo@users.noreply.github.com" && git config --system user.name "tianyagogogo"  && git pull && echo $(date) >> records.txt && git commit -a -m 'git commit '  && git push origin main
